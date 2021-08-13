e91d6ad87d6fd9eefee7be8d971eac2a0af82976
Julia shorina <juliashorina@Julias-MacBook-Air.local>
2021-08-13 14:25:56 +0200

    change foundation library to bootstrap,  navbar changed

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
-$(document).ready(function(){
-    $(document).foundation();
-  });
+// $(document).ready(function(){
+//     $(document).foundation();
+//   });
 
-  $(document).ready(function() {
-    $('.menu-icon').click(function() {
-      $('.menu-icon').toggleClass('active');
+//   $(document).ready(function() {
+//     $('.menu-icon').click(function() {
+//       $('.menu-icon').toggleClass('active');
+//     });
+//   });
+
+  $(document).ready(function () {
+    $('.custom-toggler').on('click', function () {
+  
+      $('.animated-icon').toggleClass('open');
     });
   });
