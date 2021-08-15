3367ce45ddf557a6c14334b323d7f14afb067927
Julia shorina <juliashorina@Julias-MacBook-Air.local>
2021-08-15 14:16:48 +0200

    change navbar-toggle funcioning

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
+  $(document).on('click',function(){ 
+    $('.navbar .collapse').collapse('hide');
+    showAnimationNav ();
+  })
+
+  function showAnimationNav () {
+    if ($('.navbar-toggler').hasClass( 'collapsed')) {
+      $('.animated-icon').removeClass('open');
+    } else {
+      $('.animated-icon').addClass('open');
+    }
+  }
 
   $(document).ready(function () {
-    $('.custom-toggler').on('click', function () {
-  
-      $('.animated-icon').toggleClass('open');
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("navbar-collapse in");
+        if (_opened === true && !clickover.hasClass("navbar-toggle")) {
+            $("button.navbar-toggle").click();
+        }
     });
-  });
+});
+
+
